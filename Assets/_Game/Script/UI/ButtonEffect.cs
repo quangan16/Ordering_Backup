2df81dd86b5665ca240d12952fbbebb94f023bc3
An Nguyễn <nqan1610@gmail.com>
2023-09-21 14:44:45 +0700

    add reward text in failled challenge items, fix button scale anim

--- Assets/_Game/Script/UI/ButtonEffect.cs
+++ Assets/_Game/Script/UI/ButtonEffect.cs
@@ -8,28 +8,38 @@ public class ButtonEffect : MonoBehaviour
     private float jiggleDuration = 1f;
     private float jiggleAngle = 10.0f;
     public float scaleFactor = 1.1f;
-    private Vector3 initialScale; 
+    private Vector3 initialScale;
+    private Sequence tweenSequence;
     
     void OnEnable()
     {
+        tweenSequence = DOTween.Sequence();
+      
         initialScale = transform.localScale;
         StartCoroutine(PlayEffect());
     }
 
     void OnDisable()
     {
-        transform.DOKill();
+        tweenSequence.Kill();
+    }
+
+    public void Reset()
+    {
+        transform.localScale = initialScale;
+        transform.localEulerAngles = Vector3.zero;
     }
 
     public IEnumerator PlayEffect()
     {
-        Sequence tweenSequence = DOTween.Sequence();
+        Reset();
+        tweenSequence.SetEase(Ease.InOutQuad);
         tweenSequence.Append(transform.DORotate(new Vector3(0, 0, -jiggleAngle), jiggleDuration/2))
             .Join(transform.DOScale(initialScale * scaleFactor, jiggleDuration / 2)) // Scale up
             .Append(transform.DORotate(new Vector3(0, 0, jiggleAngle), jiggleDuration))
             .Append(transform.DORotate(new Vector3(0, 0, 0), jiggleDuration/2))
             
-            .Join(transform.DOScale(initialScale, jiggleDuration / 2)); // Scale down
+            .Join(transform.DOScale(initialScale, jiggleDuration / 2)).OnComplete(() => { tweenSequence.Kill(); }); // Scale down
         // tweenSequence.Append(transform.DOScale(1.1f, 1.5f).SetEase(Ease.OutElastic)).Join(transform.).Append(
         //     transform.DOScale(1.0f, 0.2f)).SetDelay(1.0f).SetLoops(-1).OnComplete(() => { tweenSequence.Kill(); });
 
